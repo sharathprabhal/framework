69495940f103ec9c948275b78711c08c1b1a26b1
Cosmin Popovici <hellocosmin@gmail.com>
2019-07-16 20:12:15 +0300

    Add HTMLHint.

--- src/generators/output/toDisk.js
+++ src/generators/output/toDisk.js
@@ -4,6 +4,8 @@ const marked = require('marked')
 const fm = require('front-matter')
 const glob = require('glob-promise')
 const deepmerge = require('deepmerge')
+const flatten = require('lodash/flatten')
+const HTMLHint = require("htmlhint").default
 const helpers = require('../../utils/helpers')
 const stripHTML = require('string-strip-html')
 const NunjucksEnvironment = require('../../nunjucks')
@@ -22,14 +24,11 @@ module.exports = async (env, spinner) => {
   const css = await Tailwind.fromFile(globalConfig, env).catch(err => { spinner.fail('Build failed'); console.log(err); process.exit() })
   const outputDir = path.resolve(`${globalConfig.build.destination.path}`)
 
-  await fs.remove(outputDir)
-  await fs.copy(globalConfig.build.templates.source, outputDir)
-
   if (fs.pathExistsSync(globalConfig.build.assets.source)) {
     await fs.copy(globalConfig.build.assets.source, `${outputDir}/${globalConfig.build.assets.destination}`)
   }
 
-  let templates = await glob(`${outputDir}/**/*.+(${globalConfig.build.templates.filetypes || 'html|njk|nunjucks'})`)
+  let templates = await glob(`${globalConfig.build.templates.source}/**/*.+(${globalConfig.build.templates.filetypes || 'html|njk|nunjucks'})`)
 
   if (templates.length < 1) {
     throw `No "${globalConfig.build.templates.filetypes}" templates found in \`${globalConfig.build.templates.source}\`. If the path is correct, please check your \`build.templates.filetypes\` config setting.`        // return `No "${globalConfig.build.templates.filetypes}" templates found in \`${globalConfig.build.templates.source}\`. If the path is correct, please check your \`build.templates.filetypes\` config setting.`
@@ -39,12 +38,15 @@ module.exports = async (env, spinner) => {
     await fs.outputFile(`${outputDir}/css/${globalConfig.build.tailwind.css}`, css)
   }
 
+  let messages = []
+
   await helpers.asyncForEach(templates, async file => {
 
     let html = await fs.readFile(file, 'utf8')
     let frontMatter = fm(html)
     let config = deepmerge(globalConfig, frontMatter.attributes)
     let layout = config.layout || config.build.layout
+
 
     marked.setOptions({
       renderer: new marked.Renderer(),
@@ -64,11 +66,21 @@ module.exports = async (env, spinner) => {
       throw Error(`Could not render HTML for ${file}`)
     }
 
+    let lint = HTMLHint.verify(html, config.htmlhint)
+
+    if (Object.keys(lint).length > 0) {
+      lint.forEach(entry => entry.file = file);
+      messages.push(lint)
+    }
+
     html = await Transformers.process(html, config, env)
 
     let ext = config.build.destination.extension || 'html'
 
-    fs.outputFile(file, html)
+    let outPath = path.dirname(file).replace(config.build.templates.source, '')
+    let outFile = path.resolve(`${config.build.destination.path}/${outPath}/${path.basename(file, path.extname(file))}.${ext}`)
+
+    fs.outputFile(outFile, html)
       .then(() => {
         if (config.plaintext) {
           let plaintext = stripHTML(html,
@@ -81,21 +93,25 @@ module.exports = async (env, spinner) => {
             }
           })
 
-          let filepath = config.permalink || file
+          let filepath = config.permalink || outFile
           let plaintextPath = path.join(path.dirname(filepath), path.basename(filepath, path.extname(filepath)) + '.txt')
 
           fs.outputFileSync(plaintextPath, plaintext);
         }
 
         if (config.permalink) {
-          return fs.move(file, config.permalink, { overwrite: true })
+          return fs.move(outFile, config.permalink, { overwrite: true })
         }
-
-        const parts = path.parse(file)
-        fs.rename(file, `${parts.dir}/${parts.name}.${ext}`)
       })
-
   })
+
+  if (messages.length > 0) {
+    flatten(messages).forEach(msg => {
+      console.log(`\n\n❌  ${msg.message}\n   Source:   ${msg.file}\n   Offender: ${msg.evidence.trim()}`)
+    })
+    console.log('\n')
+    messages = []
+  }
 
   return templates.length
 }
